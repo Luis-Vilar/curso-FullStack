45d4aa8de3dead414f835410543fce7b31214397
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-02-09 12:22:52 -0300

    EX5

---
+++ M1S02/EX5/script.js
@@ -0,0 +1,3 @@
+function seijaAmigavel() {
+  alert("Obrigado, cliente!\nA grande missão da nossa empresa\nserá sempre a satisfação dos nossos clientes\nnosso equipe entrara em contato o mais rápido possível");
+}
