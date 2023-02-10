8db814be77b8fdb3e4edfea6811d65991da888ea
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-02-10 19:35:55 -0300

    ok

--- CODIGO DE AULAS E PRATICAS/nutricionista-main/js/main/programa.js
+++
@@ -1,8 +0,0 @@
-import { PacienteController } from "../controllers/paciente-controller.js";
-
-let controller = new PacienteController();
-let formulario = document.querySelector('.form');
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    controller.criarPaciente();
-});
