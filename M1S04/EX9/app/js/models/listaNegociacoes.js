deff119e1d64b6fd76d5cc5c04bb07fecf593464
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-02-26 20:48:29 -0300

    finalizado parte logica

--- M1S04/EX9/app/js/models/listaNegociacoes.js
+++ M1S04/EX9/app/js/models/listaNegociacoes.js
@@ -5,20 +5,16 @@ export class ListaNegociacoes {
     this.#listaNegociacoes.push(negociação);
   }
 
-  filtrarNegociacoes() {
+  filtrarNegociacoes = () => {
     var tempList = [].concat(this.#listaNegociacoes);
     let listaRetorna = [];
 
     for (let index = 0; index < tempList.length; index++) {
-      if (tempList[index].valor > 5000 ) {
-        listaRetorna.push(tempList[index])
-        
+      if (tempList[index].valor > 5000) {
+        listaRetorna.push(tempList[index]);
       }
-    
     }
 
     return listaRetorna;
-  }
- 
-
+  };
 }
