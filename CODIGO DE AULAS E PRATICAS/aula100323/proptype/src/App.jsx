5222a6d8ec3aa4b31dd53c7027ebd5872d7429f5
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-03-10 23:20:32 -0300

    ok

--- CODIGO DE AULAS E PRATICAS/aula100323/proptype/src/App.jsx
+++ CODIGO DE AULAS E PRATICAS/aula100323/proptype/src/App.jsx
@@ -4,7 +4,10 @@ import Imagem from "./components/Imagem";
 function App() {
   return (
     <div className="App">
-      <Imagem alternativa="Gato" camino="https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA"/>
+      <Imagem
+        alternativa="Gato"
+        camino="https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA"
+      />
     </div>
   );
 }
