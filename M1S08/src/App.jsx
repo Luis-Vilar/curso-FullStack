decf215c78e94772c152078963e0dc673807b7d9
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-03-22 19:08:59 -0300

    EX-3

--- M1S08/src/App.jsx
+++ M1S08/src/App.jsx
@@ -2,9 +2,12 @@
 import './App.css'
 import React from 'react'
 import CardAdicionar from './components/CardAdicionar'
+import { useState } from 'react'
 // inicio Componente
 function App() {
-  
+  //variaveis
+  const [listaTarefs, setListaTarefas] = useState([])
+
   return (
     <div className="App">
       {/* componentes externos*/}
