3fc0930902cae9e3e00fec5e83bfd9d41abfda9d
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-03-22 14:52:15 -0300

    ex -4

--- M1S08/src/App.jsx
+++ M1S08/src/App.jsx
@@ -1,12 +1,31 @@
 import CardAdicionar from "./components/CardAdicionar";
 import{useState} from 'react';
+
 function App() {
+
 const [tarefas, settarefas] = useState([]);
+
+function criarTarefa(textoTarefa){
+  const novaTarefa = {
+    id: Date.now(),
+    texto: textoTarefa,
+    finalizado: false
+  }
+  settarefas([...tarefas, novaTarefa]);
+}
+
   return (
     <div className="App">
-      <CardAdicionar/>
+      <CardAdicionar criarTarefa={criarTarefa}/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
