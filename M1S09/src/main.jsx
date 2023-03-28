2bcd8d6ddde4563b96fe12e841b9d5546ef7ac64
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-03-28 16:44:49 -0300

    ok

--- M1S09/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { appRouter } from "./Router";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={appRouter} />
-);
