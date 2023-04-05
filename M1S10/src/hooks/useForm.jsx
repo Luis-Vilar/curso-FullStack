d482e954493cec9ebada55aba6f6f5da5c17c1da
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-04-05 17:25:02 -0300

    add custom hook y estilos

---
+++ M1S10/src/hooks/useForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export function useForm(formularios) {
+    const [passo, setPasso] = useState(0)
+
+    function mudarPasso(valor, evento) {
+        evento.preventDefault()
+
+        if(valor < 0 || valor >= formularios.length ){
+            return
+        }
+
+        setPasso(valor)
+    }
+
+
+    return {
+        passoAtual: passo, 
+        componenteAtual: formularios[passo], 
+        mudarPasso, 
+        ehUltimoPasso: passo + 1 == formularios.length ? true : false
+    }
+}
