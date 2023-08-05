a1a9df7644b56ce6e3c896d16b632fdda6857c98
Luis-Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-08-04 21:12:24 -0300

    ex6 ok

--- M3S01/ex6.js
+++ M3S01/ex6.js
@@ -14,31 +14,48 @@ class Estoque {
     adicionarProduto(produto) {
         this.produtos.push(produto);
     }
-
     removerProduto(produto) {
         const index = this.produtos.findIndex((p) => p.nome === produto.nome);
         if (index !== -1) {
             this.produtos.splice(index, 1);
         }
     }
-
     listarProdutos() {
         return this.produtos;
     }
 }
+/**
+ * As responsabilidades foram separadas em classes distintas, 
+ * onde a classe Estoque gerencia apenas os produtos em estoque 
+ * e a nova classe OperacoesEstoque é responsável por realizar
+ *  as operações de adicionar e remover produtos.
+ * O código agora está mais extensível, pois novas funcionalidades 
+ * relacionadas ao estoque podem ser adicionadas sem modificar a classe Estoque,
+ * com isso agora cumpre com os princípios da responsabilidade unica e aberto fechado
+ * do SOLID.
+ */
 
-// Utilização do Estoque
+class OperacoesEstoque {
+    adicionarProduto(estoque, produto) {
+        estoque.adicionarProduto(produto);
+    }
+    removerProduto(estoque, produto) {
+        estoque.removerProduto(produto);
+    }
+}
+
 const estoque = new Estoque();
+const operacoesEstoque = new OperacoesEstoque();
 
 const produto1 = new Produto('Camiseta', 29.99, 50);
-estoque.adicionarProduto(produto1);
-
+operacoesEstoque.adicionarProduto(estoque, produto1);
 const produto2 = new Produto('Calça Jeans', 89.99, 30);
-estoque.adicionarProduto(produto2);
+operacoesEstoque.adicionarProduto(estoque, produto2);
 
 console.log('Produtos em estoque:', estoque.listarProdutos());
 
-estoque.removerProduto(produto1);
+// injeção de dependência para fornecer a instância da classe Estoque à classe OperacoesEstoque
+operacoesEstoque.removerProduto(estoque, produto1);
 
 console.log('Produtos em estoque após a remoção:', estoque.listarProdutos());
 
